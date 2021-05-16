a51321f9bd4b0146716c8823cb98a1616c783cde
Biswas <Subhajit.Biswas@transformco.com>
2021-05-16 20:35:46 +0530

    Algorithm practice for 16/5/2021

---
+++ src/main/java/com/problems/algorithm/search/LinearSearch.java
@@ -0,0 +1,10 @@
+package com.problems.algorithm.search;
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/com/problems/algorithm/sort/InsertionSort.java
+++ src/main/java/com/problems/algorithm/sort/InsertionSort.java
@@ -1,4 +1,4 @@
-package com.problems.algorithm;
+package com.problems.algorithm.sort;
 
 /*
 Insertion Sort
