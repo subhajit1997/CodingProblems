b4db85e62bcb65cc5ec6948fcbbbb6d911b0b848
Subhajit Biswas <s.biswas1@elsevier.com>
2022-09-09 02:05:49 +0530

    recursion code pracise

---
+++ src/main/java/com/problems/course/udmey/java_dsa_masterclass/_2PowerOfNumber.java
@@ -0,0 +1,28 @@
+package com.problems.course.udmey.java_dsa_masterclass;
+
+/**
+ * @author biswass7
+ * @) How to calculate power of a number using recursion
+ * 
+ * url: https://relxlearning.udemy.com/course/java-data-structures-and-algorithms-masterclass/learn/lecture/23272228#notes
+ */
+
+public class _2PowerOfNumber {
+
+	public static void main(String[] args) {
+		_2PowerOfNumber p =new _2PowerOfNumber();
+		var result=p.power(4,5);
+		System.out.println(result);
+
+	}
+
+	private int power(int base, int exp) {
+		if(exp<0)
+			return -1;
+		if(exp==0)
+			return 1;
+		return base* power(base, exp-1);
+		
+	}
+
+}
