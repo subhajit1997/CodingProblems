3ce1a7c53a6ca9f16504f5aa2fe588d94326be42
Subhajit Biswas <s.biswas1@elsevier.com>
2022-11-24 23:37:22 +0530

    added new code

--- src/main/java/com/problems/course/udmey/java_dsa_masterclass/section4/Exercise8.java
+++ src/main/java/com/problems/course/udmey/java_dsa_masterclass/section4/Exercise8.java
@@ -1,12 +1,15 @@
 package com.problems.course.udmey.java_dsa_masterclass.section4;
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /**
  * @author biswass7
  *
  *someRecursive
-Write a recursive function called someRecursive which accepts an array and a callback. The function returns true if a single value in the array returns true when passed to the callback. Otherwise it returns false.
+Write a recursive function called someRecursive which accepts an array and a callback. The function returns true if a
+single value in the array returns true when passed to the callback. Otherwise it returns false.
 
 Examples
 
@@ -18,12 +21,16 @@ someRecursive({4,6,8}, isOdd) # false
 public class Exercise8 {
 	public static void main(String[] args) {
 		OddFunction odd = new OddFunction();
-		System.out.println(someRecursive(new int[] {1,2,3,4},odd));
+		System.out.println(someRecursive(new int[] {4,6,8},odd));
 	}
 
 	private static boolean someRecursive(int[] arr, OddFunction odd) {
-		// TODO Auto-generated method stub
-		return true;
+		if(arr.length == 0)
+            return false;
+        else if(!odd.run(arr[0]))
+            return someRecursive(Arrays.copyOfRange(arr,1,arr.length),odd);
+        else
+            return true;
 	}
 
 	
---
+++ src/main/java/com/problems/course/udmey/java_dsa_masterclass/section4/Exercise9.java
@@ -0,0 +1,31 @@
+package com.problems.course.udmey.java_dsa_masterclass.section4;
+
+
+
+/**
+ * @author biswass7
+ *
+ *First Uppercase
+ * Given a string find its first uppercase letter
+ *
+ * Example
+ *
+ * Input : appmillerS
+ * Output : S
+
+ */
+public class Exercise9 {
+	public static void main(String[] args) {
+		System.out.println(first("appmillerS"));
+	}
+
+	private static char first(String str) {
+		if(str.isEmpty() || str=="null")
+			return ' ';
+		char firstChar = str.charAt(0);
+		if(Character.isUpperCase(firstChar))
+			return firstChar;
+		else
+			return first(str.substring(1,str.length()));
+	}
+}
